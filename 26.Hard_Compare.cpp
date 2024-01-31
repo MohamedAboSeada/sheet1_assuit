299bf7e03d3db419b0c3b6c5e463f6ded9e5501e
MohamedAboSeada <madoaxel287@gmail.com>
2024-01-31 21:34:19 +0200

    updated

--- 26.Hard_Compare.cpp
+++ 26.Hard_Compare.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <cmath>
+#define ll long long
 using namespace std;
 
 int main(){
-
-
+    ll a,b,c,d;
+    cin>>a>>b>>c>>d; 
+    (b * log(a) > d * log(c)) ? cout<<"YES\n" : cout<<"NO\n";
     return 0;
 }
